dcd3a02378711107bc9dd37cfacd2f287fb8e606
Zandiegho <jd90099@gmail.com>
2023-05-23 21:17:28 -0500

    Validacion middleware

--- app/Http/Middleware/ValidarRolAdmin.php
+++ app/Http/Middleware/ValidarRolAdmin.php
@@ -12,6 +12,9 @@ class ValidarRolAdmin
 
     public function handle($request, Closure $next)
     {
+        /* el aut -> user toma los datos del usario por lo que si validamos, el rol id de la tabla
+        users encontramos los permitidos */
+
         if (auth()->user()->rol_id == 1) {
             return $next($request);
         }
